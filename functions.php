26228be6d728179aa183aa2f2dd859844b2a0c0d
Code IT Hub <78258092+codeithub@users.noreply.github.com>
2021-07-05 01:43:31 +0530

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,5 @@
+add_action( 'woocommerce_thankyou', 'codeithub_add_content_thankyou' );
+  
+function codeithub_add_content_thankyou() {
+   echo '<p>Add Content to the WooCommerce Thank You Page</p>';
+}
